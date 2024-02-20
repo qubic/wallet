9ef09ccb5ef30c72b839f947951ac8faf68dad9d
AndyQus <AndyQus@outlook.com>
2024-02-20 18:11:26 +0100

    new version 3.2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    version: 3.1,
+    version: 3.2,
     production: true,
     apiUrl: 'https://api.qubic.li',
     explorer:'https://app.qubic.li/network/explorer/address/',
