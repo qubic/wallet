9e7cb3f24d5ad0609cc83ef3de0d023953bbaf6b
AndyQus <AndyQus@outlook.com>
2024-03-04 22:16:34 +0100

    android apk app is created

---
+++ deploy.bat
@@ -0,0 +1,4 @@
+@echo off
+
+rem ng build
+copy /Y /S dist\qubic-wallet\* cordova\www
---
+++ deploy.ps1
@@ -0,0 +1,6 @@
+# Set the source and destination directories
+$SourceDirectory = "dist\qubic-wallet\*"
+$DestinationDirectory = "cordova\www"
+
+# Copy files and subdirectories
+Copy-Item -Path $SourceDirectory -Destination $DestinationDirectory -Recurse -Force
