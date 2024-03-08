48058888052181867853cb1affb91313e6ba3a7b
AndyQus <AndyQus@outlook.com>
2024-03-08 22:44:41 +0100

    remove build files and chaned the deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,8 @@
 # Set the source and destination directories
 $SourceDirectory = "dist\qubic-wallet\*"
-$DestinationDirectory = "cordova\www"
+$DestinationDirectory = "cordova\www\app"
+
+Remove-Item .\$SourceDirectory -Recurse -Force  
 
 # Copy files and subdirectories
 Copy-Item -Path $SourceDirectory -Destination $DestinationDirectory -Recurse -Force
