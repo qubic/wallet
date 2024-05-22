80377252cce63c063f7db2d30274ec66209e5fa3
AndyQus <AndyQus@outlook.com>
2024-05-22 21:22:43 +0200

    tick-error resetCounter

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -135,7 +135,7 @@ export class NavigationComponent implements OnInit {
     if (this.intervalId) {
       clearInterval(this.intervalId);
       this.intervalId = null;
-      this.currentTickSec = 0;
+      this.resetCounter();
     }
   }
 
