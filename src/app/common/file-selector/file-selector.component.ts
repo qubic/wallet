ce6e2f7d2160f9ef144c7087bde7d6120cca1d94
AndyQus <AndyQus@outlook.com>
2024-05-30 22:24:18 +0200

    drag and drop: You can now drag the file to the corresponding area/button.

--- src/app/common/file-selector/file-selector.component.ts
+++ src/app/common/file-selector/file-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output, HostListener } from '@angular/core';
 
 
 
@@ -8,23 +8,45 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrls: ['./file-selector.component.scss']
 })
 export class FileSelectorComponent {
+  @Input() requredText: string = 'File required';
+  @Input() selectFileText: string = 'Select file';
+  @Output() fileSelected = new EventEmitter<File>();
 
-  public selectedFile: File | undefined = undefined;
+  selectedFile: File | undefined = undefined;
+  isDragOver = false;
 
-  @Input()
-  public requredText: string = 'File required';
+  @HostListener('dragover', ['$event'])
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragOver = true;
+  }
 
-  @Input()
-  public selectFileText: string = 'Select file';
+  @HostListener('dragleave', ['$event'])
+  onDragLeave(event: DragEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragOver = false;
+  }
 
-  @Output()
-  public fileSelected: EventEmitter<File> = new EventEmitter<File>();
+  @HostListener('drop', ['$event'])
+  async onDrop(event: DragEvent): Promise<void> {
+    event.preventDefault();
+    event.stopPropagation();
+    this.isDragOver = false;
 
-  onFileSelected(event: any) {
-    this.selectedFile = event?.target.files[0];
-    if(this.selectFileText){
-      this.fileSelected.emit(this.selectedFile!);
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      this.onFileSelected({ target: { files: [file] } });
     }
   }
 
+  onFileSelected(event: any) {
+    const file = event?.target?.files[0];
+    if (file) {
+      this.selectedFile = file;
+      this.fileSelected.emit(file);
+    }
+  }
 }
