3d04947266f83c665403e1ccd68954249065d07d
AndyQus <AndyQus@outlook.com>
2024-06-02 23:41:07 +0200

    new branch release/4.4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    version: 4.3,
+    version: 4.4,
     production: true,
     apiUrl: 'https://api.qubic.li',
     explorer:'https://explorer.qubic.org/network/address/',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    version: 4.3,
+    version: 4.4,
     production: false,
     apiUrl: 'http://localhost:7003', 
     explorer:'https://explorer.qubic.org/network/address/',
