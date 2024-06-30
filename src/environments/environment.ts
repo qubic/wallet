9b564ede9cf63fbe16871cfb79d5a8d34a568b29
AndyQus <AndyQus@outlook.com>
2024-06-30 17:06:42 +0200

    created new branch release/4.6

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    version: 4.5,
+    version: 4.6,
     production: false,
     apiUrl: 'http://localhost:7003', 
     explorer:'https://explorer.qubic.org/network/address/',
