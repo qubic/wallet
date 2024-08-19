033fd3e2ff4f81d45f03fdbde59002b42ead7da4
realworld777 <discod.dream120@gmail.com>
2024-08-19 13:40:20 +0900

    feat: migrating reward table

--- src/app/qearn/reward-table/reward-table.component.ts
+++ src/app/qearn/reward-table/reward-table.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { REWARD_DATA } from './table-data';
 
 export interface WeekData {
   weekFrom: number;
@@ -8,23 +9,6 @@ export interface WeekData {
   boost: number;
 }
 
-const ELEMENT_DATA: WeekData[] = [
-  { weekFrom: 0, weekTo: 3, earlyUnlock: 0, burn: 0, boost: 100 },
-  { weekFrom: 4, weekTo: 7, earlyUnlock: 5, burn: 45, boost: 50 },
-  { weekFrom: 8, weekTo: 11, earlyUnlock: 5, burn: 45, boost: 50 },
-  { weekFrom: 12, weekTo: 15, earlyUnlock: 10, burn: 45, boost: 45 },
-  { weekFrom: 16, weekTo: 19, earlyUnlock: 15, burn: 40, boost: 45 },
-  { weekFrom: 20, weekTo: 23, earlyUnlock: 20, burn: 40, boost: 40 },
-  { weekFrom: 24, weekTo: 27, earlyUnlock: 25, burn: 35, boost: 40 },
-  { weekFrom: 28, weekTo: 31, earlyUnlock: 30, burn: 35, boost: 35 },
-  { weekFrom: 32, weekTo: 35, earlyUnlock: 35, burn: 35, boost: 30 },
-  { weekFrom: 36, weekTo: 39, earlyUnlock: 40, burn: 30, boost: 30 },
-  { weekFrom: 40, weekTo: 43, earlyUnlock: 45, burn: 30, boost: 25 },
-  { weekFrom: 44, weekTo: 47, earlyUnlock: 50, burn: 30, boost: 20 },
-  { weekFrom: 48, weekTo: 51, earlyUnlock: 55, burn: 25, boost: 20 },
-  { weekFrom: 52, weekTo: 52, earlyUnlock: 100, burn: 0, boost: 0 }
-];
-
 @Component({
   selector: 'app-reward-table',
   templateUrl: './reward-table.component.html',
@@ -32,5 +16,5 @@ const ELEMENT_DATA: WeekData[] = [
 })
 export class RewardTableComponent {
   displayedColumns: string[] = ['weekFrom', 'weekTo', 'earlyUnlock', 'burn', 'boost'];
-  dataSource = ELEMENT_DATA;
+  dataSource = REWARD_DATA;
 }
