61bf7ec982535e4b76dad77b59290ff541e608c2
AndyQus <AndyQus@outlook.com>
2024-09-07 12:51:17 +0200

    api models/interfaces of the various APIs created, new folder structure

---
+++ src/app/services/apis/live/api.live.model.ts
@@ -0,0 +1,134 @@
+// https://qubic.github.io/integration/Partners/qubic-rpc-doc.html?urls.primaryName=Qubic%20RPC%20Live%20Tree
+
+// /assets/{identity}/issued
+export interface IssuedAssetsResponse {
+    issuedAssets: {
+        data: {
+            issuerIdentity: string;
+            type: number;
+            name: string;
+            numberOfDecimalPlaces: number;
+            unitOfMeasurement: number[];
+        };
+        info: {
+            tick: number;
+            universeIndex: number;
+        };
+    }[];
+}
+
+
+// /assets/{identity}/owned
+export interface OwnedAssetsResponse {
+    ownedAssets: {
+        data: {
+            ownerIdentity: string;
+            type: number;
+            padding: number;
+            managingContractIndex: number;
+            issuanceIndex: number;
+            numberOfUnits: string;
+            issuedAsset: {
+                issuerIdentity: string;
+                type: number;
+                name: string;
+                numberOfDecimalPlaces: number;
+                unitOfMeasurement: number[];
+            };
+        };
+        info: {
+            tick: number;
+            universeIndex: number;
+        };
+    }[];
+}
+
+
+// /assets/{identity}/possessed
+export interface PossessedAssetsResponse {
+    possessedAssets: {
+        data: {
+            possessorIdentity: string;
+            type: number;
+            padding: number;
+            managingContractIndex: number;
+            issuanceIndex: number;
+            numberOfUnits: string;
+            ownedAsset: {
+                ownerIdentity: string;
+                type: number;
+                padding: number;
+                managingContractIndex: number;
+                issuanceIndex: number;
+                numberOfUnits: string;
+                issuedAsset: {
+                    issuerIdentity: string;
+                    type: number;
+                    name: string;
+                    numberOfDecimalPlaces: number;
+                    unitOfMeasurement: number[];
+                };
+            };
+        };
+        info: {
+            tick: number;
+            universeIndex: number;
+        };
+    }[];
+}
+
+
+// /balances/{id}
+export interface BalanceResponse {
+    balance: {
+        id: string;
+        balance: string;
+        validForTick: number;
+        latestIncomingTransferTick: number;
+        latestOutgoingTransferTick: number;
+        incomingAmount: string;
+        outgoingAmount: string;
+        numberOfIncomingTransfers: number;
+        numberOfOutgoingTransfers: number;
+    };
+}
+
+
+// /block-height
+export interface BlockHeightResponse {
+    blockHeight: {
+        tick: number;
+        duration: number;
+        epoch: number;
+        initialTick: number;
+    };
+}
+
+
+// /broadcast-transaction
+export interface BroadcastTransactionResponse {
+    peersBroadcasted: number;
+    encodedTransaction: string;
+    transactionId: string;
+}
+
+
+// /querySmartContract
+export interface QuerySmartContractResponse {
+    contractIndex: number;
+    inputType: number;
+    inputSize: number;
+    requestData: string;
+  }
+  
+
+  // /tick-info
+  export interface TickInfoResponse {
+    tickInfo: {
+      tick: number;
+      duration: number;
+      epoch: number;
+      initialTick: number;
+    };
+  }
+  
