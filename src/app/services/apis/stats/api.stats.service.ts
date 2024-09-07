304f40f08fbb3a0714619e0e4527e180d8211742
AndyQus <AndyQus@outlook.com>
2024-09-07 13:20:11 +0200

    new function getRichList() in api.stats.service

--- src/app/services/apis/stats/api.stats.service.ts
+++ src/app/services/apis/stats/api.stats.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { LatestStatsResponse } from './api.stats.model';
+import { LatestStatsResponse, RichListRespone } from './api.stats.model';
 import { HttpClient, HttpContext } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { map, Observable, of } from 'rxjs';
@@ -9,15 +9,15 @@ import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
 @Injectable({
     providedIn: 'root'
 })
-export class ApiStatsService {
 
+export class ApiStatsService {
     public currentProtocol: BehaviorSubject<number> = new BehaviorSubject<number>(0);
     private basePath = environment.apiArchiverUrl;
 
-    constructor(protected httpClient: HttpClient,) {
-
+    constructor(protected httpClient: HttpClient) {
     }
 
+    
     public getLatestStats() {
         let localVarPath = `/v1/latest-stats`;
         return this.httpClient.request<LatestStatsResponse>('get', `${this.basePath}${localVarPath}`,
@@ -27,6 +27,26 @@ export class ApiStatsService {
             }
         ).pipe(
             map((response: LatestStatsResponse) => {
+                if (response) {
+                    //console.log('Response from getStatus:', response);
+                    return response;
+                } else {
+                    throw new Error('Invalid response format');
+                }
+            })
+        );
+    }
+
+
+    public getRichList() {
+        let localVarPath = `/v1/rich-list`;
+        return this.httpClient.request<RichListRespone>('get', `${this.basePath}${localVarPath}`,
+            {
+                context: new HttpContext(),
+                responseType: 'json'
+            }
+        ).pipe(
+            map((response: RichListRespone) => {
                 if (response) {
                     //console.log('Response from getStatus:', response);
                     return response;
