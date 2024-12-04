9e4e2a51cc6d005b0a5e95c49ce82dd2f5b8a52b
AndyQus <AndyQus@outlook.com>
2024-12-04 10:02:39 +0100

    Reduce asset fees to 100 in production and development environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
     apiQliUrl: 'https://api.qubic.li',
     apiUrl: 'https://rpc.qubic.org',
     explorer:'https://explorer.qubic.org/network/address/',
-    assetsFees: 1000000
+    assetsFees: 100
   };
 
